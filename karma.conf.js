602b315769774ca04ea6e23797cb13ff32fbaa21
Jakob Blume <jakob.blume@mailbox.org>
2017-05-24 02:35:56 +0200

    update

--- karma.conf.js
+++ karma.conf.js
@@ -1,15 +1,16 @@
 module.exports = function(config) {
   config.set({
     browsers: ['Chrome'],
-    frameworks: ['jasmine-jquery', 'jasmine'],
+    frameworks: ['jasmine-jquery', 'jasmine', 'jasmine-matchers'],
     plugins: [
       'karma-chrome-launcher',
       'karma-jasmine-jquery',
-      'karma-jasmine'
+      'karma-jasmine',
+      'karma-jasmine-matchers'
     ],
     files: [
-      'node_modules/jquery/dist/jquery.min.js',
-      '*/static/**/*.js',
+      '*/static/jugendhaus/js/lib/jquery-3.2.1.min.js',
+      '*/static/jugendhaus/js/*.js',
       '*/tests/**/*.spec.js',
       { pattern: '*/tests/**/*.html', included: false, served: true }
     ],
